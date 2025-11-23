d83177bfeea7964d0ffbde73975891a2485ff8bd
VKH <v.v.hudenko@gmail.com>
2025-11-23 12:13:53 +0200

    tmp commit

--- src/TmpMonitor.ino
+++ src/TmpMonitor.ino
@@ -10,8 +10,16 @@
 
 #define configFilePath "/config.json"
 
+#define MaxMeasurementsCount 60
+
 #define uS_TO_S_FACTOR 1000000ULL
-#define TIME_TO_SLEEP 30 // seconds
+#define TIME_TO_SLEEP 60 // seconds
+
+struct TmpMeasurement
+{
+    double temperature;
+    char *time;
+};
 
 String WIFI_SSID = "";
 String WIFI_PASSWORD = "";
@@ -36,34 +44,9 @@ RTC_DATA_ATTR tm spreadsheetTabCreated;
 
 RTC_DATA_ATTR int lastRow = 0;
 
-// void scanMultiplexerAddresses()
-// {
-//     Wire.begin();
-//     if (multiplexer.begin() == false)
-//     {
-//         Log.info("COULD NOT CONNECT TO MULTIPLEXER \n");
+RTC_DATA_ATTR int measurementIndex = 0;
 
-//     for (uint8_t channel = 0; channel < 8; channel++)
-//     {
-//         multiplexer.enableChannel(channel);
-//         Log.info("TCA channel #%d\n", channel);
-
-//         for (uint8_t addr = 0; addr <= 127; addr++)
-//         {
-//             if (addr == multiplexerAddress)
-//                 continue;
-
-//             Wire.beginTransmission(addr);
-//             if (!Wire.endTransmission())
-//             {
-//                 Log.info("Found I2C 0x%x\n", addr);
-//             }
-//         }
-
-//         multiplexer.disableChannel(channel);
-//     }
-//     Log.info("Done scan multiplexer addresses\n");
-// }
+RTC_DATA_ATTR TmpMeasurement temperatures[100];
 
 void setup()
 {
@@ -121,76 +104,41 @@ void loop()
     bool ready = GSheet.ready();
     if (ready)
     {
-        digitalWrite(routerPowerOnPin, HIGH);
-        delay(1000 * 30);
-
-        // createGoogleSheet();
-
-        createGoogleSheetTab();
-
-        if (lastRow == 0)
+        if (measurementIndex >= MaxMeasurementsCount - 1)
         {
-            readLastRow();
+            digitalWrite(routerPowerOnPin, HIGH);
+            delay(1000 * 30);
+
+            createGoogleSheetTab();
+
+            if (lastRow == 0)
+            {
+                readLastRow();
+            }
+
+            readAndRememberTemperature();
+            logTemperature();
+
+            digitalWrite(routerPowerOnPin, LOW);
+            esp_deep_sleep_start();
         }
-
-        double temperature = readTemperature();
-        logTemperature(temperature);
-
-        digitalWrite(routerPowerOnPin, LOW);
-        esp_deep_sleep_start();
+        else
+        {
+            readAndRememberTemperature();
+            //esp_deep_sleep_start();
+        }
     }
 
     delay(500);
 }
 
-// void makeMotorShaftOneTurnTask()
-// {
-//     multiplexer.enableChannel(1);
-
-//     if (millis() - as5600.encoderTimer > 125) // 125 ms will be able to make 8 readings in a sec which is enough for 60 RPM
-//     {
-//         as5600.readRawAngle();  // ask the value from the sensor
-//         as5600.correctAngle();  // tare the value
-//         as5600.checkQuadrant(); // check quadrant, check rotations, check absolute angular position
-
-//         as5600.encoderTimer = millis();
-
-//         if (currentTurns != as5600.numberofTurns)
-//         {
-//             currentTurns = as5600.numberofTurns;
-//             motor.stop();
-//             delay(1000);
-
-//             motor.move();
-
-//             Log.info("Turns: %d\n", currentTurns);
-//             Log.info("Speed: %F\n", as5600.rpmValue);
-//         }
-
-//         /*A little brainstorm on determining the required delay
-//          * The above 3 functions require about 300-310 us to finish
-//          * They mess up the interrupt of the CNC encoder due to the i2C communication
-//          * Therefore it is not good if they are called very often
-//          * We want to detect at least every rotations of the shaft
-//          * I say (arbitrarily), that we need to detect at least 2 angles in each quadrants, so in 1 turn of the shaft, there are 8 readings
-//          * 8 readings per turn can be converted into readings per second based on the expected highest speed
-//          * Example:
-//          * 60 RPM = 60/60 RPS (rounds per seconds) = 1 RPS
-//          * 1 round per second -> 8 reading per second -> 1 second/8 readings = 0.125 s = 125 ms is the frequency of readings
-//          *
-//          * Example 2:
-//          *
-//          * 100 RPM = 100/60 = 1.667 RPS
-//          * 1 round = 0.599 s -> 0.599 s/ 8 readings = 74.98 ~ 75 ms.
-//          * Check: 60/100 = 0.6 -> 75/125 = 0.6.
-//          */
-//     }
-
-//     as5600.calculateRPMTask(); // calculate the RPM
-// }
-
-double readTemperature()
+TmpMeasurement readTemperature()
 {
+    tm now = getTimeNow();
+
+    char timeString[20];
+    strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &now);
+
     int adcValue = analogRead(thermistorPin);                       // read ADC pin
     double voltage = (float)adcValue / 4095.0 * 3.3;                // calculate voltage
     double Rt = 10 * voltage / (3.3 - voltage);                     // calculate resistance value of thermistor
@@ -198,7 +146,22 @@ double readTemperature()
     double tempC = tempK - 273.15;                                  // calculate temperature (Celsius)
     Serial.printf("Pin value : %d,\tVoltage : %.2fV, \tTemperature : %.2fC\n", adcValue, voltage, tempC);
 
-    return tempC;
+    return {tempC, timeString};
+}
+
+void addTemperatureToMemory(TmpMeasurement temperature)
+{
+    if (measurementIndex < 100)
+    {
+        temperatures[measurementIndex] = temperature;
+        measurementIndex++;
+    }
+}
+
+void readAndRememberTemperature()
+{
+    TmpMeasurement temperature = readTemperature();
+    addTemperatureToMemory(temperature);
 }
 
 void connectToWiFi()
@@ -247,25 +210,38 @@ void setupGoogleSheet()
     Log.infoln("Google Sheet client setup ready");
 }
 
-void logTemperature(double temperature)
+void logTemperature()
 {
     Log.infoln("Log temperature task started");
 
     if (!spreadsheetId.isEmpty())
     {
-        tm now = getTimeNow();
+        // tm now = getTimeNow();
 
-        char timeString[50];
-        strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &now);
+        // char timeString[50];
+        // strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &now);
 
         char range[50]; // Ensure the buffer is large enough
-        sprintf(range, "%s!A%d:B%d", spreadsheetTab, lastRow + 1, lastRow + 1);
+        int newLastRow = lastRow + MaxMeasurementsCount;
+        sprintf(range, "%s!A%d:B%d", spreadsheetTab, newLastRow, newLastRow);
 
         FirebaseJson valueRange;
         valueRange.add("range", range);
         valueRange.add("majorDimension", "ROWS");
-        valueRange.set("values/[0]/[0]", timeString);
-        valueRange.set("values/[0]/[1]", temperature); // row 1/ column 2
+
+        for (int i = 0; i < sizeof(temperatures); i++)
+        {
+            TmpMeasurement measurement = temperatures[i];
+
+            char col1Path[20];
+            sprintf(col1Path, "values/[%d]/[0]", i);
+
+            char col2Path[20];
+            sprintf(col2Path, "values/[%d]/[1]", i);
+
+            valueRange.set(col1Path, measurement.time);
+            valueRange.set(col2Path, measurement.temperature); // row 1/ column 2
+        }
 
         FirebaseJson response;
         bool success = GSheet.values.update(&response, spreadsheetId, range, &valueRange);
@@ -274,7 +250,8 @@ void logTemperature(double temperature)
 
         if (success)
         {
-            lastRow++;
+            lastRow = newLastRow;
+            measurementIndex = 0;
 
             // Serial.println("\nGet spreadsheet values...");
             // Serial.println("------------------------------");
@@ -303,62 +280,6 @@ tm getTimeNow()
 
     return timeinfo;
 }
-
-// void createGoogleSheet()
-// {
-//     tm now = getTimeNow();
-
-//     if (!spreadsheetId.isEmpty() && now.tm_mday == spreadsheetTabCreated.tm_mday)
-//     {
-//         return;
-//     }
-
-//     FirebaseJson response;
-
-//     Serial.println("\nCreate spreadsheet...");
-//     Serial.println("------------------------");
-
-//     Log.infoln("Creating new spreadsheet for user: %s", GOOGLE_USER_EMAIL);
-
-//     // strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo);
-
-//     // FirebaseJson spreadsheet;
-//     // spreadsheet.set("properties/title", "BasementTmp_" + now.tm_year + now.tm_mon + now.tm_mday);
-//     // spreadsheet.set("sheets/properties/gridProperties/rowCount", 2000);
-//     // spreadsheet.set("sheets/properties/gridProperties/columnCount", 2);
-//     // spreadsheet.set("sheets/properties/gridProperties/columnCount", 2);
-
-//     // // For Google Sheet API ref doc, go to https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/create
-//     // bool success = GSheet.create(&response, &spreadsheet, GOOGLE_USER_EMAIL);
-//     String fileName = "BasementTmp_" + now.tm_year + now.tm_mon + now.tm_mday;
-//     bool success = GSheet.createEmpty(&response, fileName, "1W8HnOToJvbgOWrHg_5CZWhRhjTwWsAlD");
-
-//     response.toString(Serial, true);
-//     Serial.println();
-
-//     if (success)
-//     {
-//         // Get the spreadsheet id from already created file.
-//         FirebaseJsonData result;
-//         response.get(result, FPSTR("spreadsheetId")); // parse or deserialize the JSON response
-//         if (result.success)
-//         {
-//             spreadsheetId = result.to<const char *>();
-//         }
-
-//         // Get the spreadsheet URL.
-//         result.clear();
-//         response.get(result, FPSTR("spreadsheetUrl")); // parse or deserialize the JSON response
-//         if (result.success)
-//         {
-//             String spreadsheetURL = result.to<const char *>();
-//             Serial.println("\nThe spreadsheet URL");
-//             Serial.println(spreadsheetURL);
-//         }
-
-//         spreadsheetTabCreated = now;
-//     }
-// }
 
 void createGoogleSheetTab()
 {
@@ -408,6 +329,7 @@ void createGoogleSheetTab()
         strcpy(spreadsheetTab, tabName);
         spreadsheetTabCreated = now;
         lastRow = 0;
+        measurementIndex = 0;
     }
     else
     {
